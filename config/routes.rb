4e97d705ed2d4366264513935c00921e1107dbd2
zmateusz <mat309@o2.pl>
2015-07-31 01:25:43 +0200

    enabled facebook OAauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,6 +13,8 @@ Rails.application.routes.draw do
   get 'pages/demo'
   post 'pages/demo'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
