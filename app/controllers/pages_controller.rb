d07d511069eb57c226e825afb167ca16346fbfba
zmateusz <mat309@o2.pl>
2015-08-02 01:26:29 +0200

    updated demo controller and views, v1.0 ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,14 @@ class PagesController < ApplicationController
   def home; end
 
   def demo
-    @question = ""
-    if !params[:content].nil?
-      @cords = Hash.new
-      result = Geocoder.search(params[:content])
-      unless result[0].nil?
-        @cords[:lat] = result[0].geometry["location"]["lat"]
-        @cords[:lng] = result[0].geometry["location"]["lng"]
+    @demo_search = ""
+    if (params[:location_a].present? && params[:location_b].present?)
+      a = params[:location_a]
+      b = params[:location_b]
+      coordinates_a, coordinates_b = Track.new(location_a: a, location_b: b).get_coordinates
+      unless (coordinates_a.nil? && coordinates_b.nil?)
+        @distance = Geocoder::Calculations.distance_between(coordinates_a, coordinates_b).round(2)
+        @track = Track.new(location_a: a, location_b: b)
       end
     end
   end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,9 +1,9 @@
 module TracksHelper
-  def static_map(track)
+  def static_map(track, position)
     a, b = track.get_coordinates
-    image_tag "http://maps.google.com/maps/api/staticmap?size=500x400&sensor=false&maptype=hybrid\
+    image_tag "http://maps.google.com/maps/api/staticmap?size=550x450&sensor=false&maptype=hybrid\
               &path=color:0x0000ff|#{a[0]},#{a[1]}|#{b[0]},#{b[1]}\
               &markers=color:blue%7Clabel:A%7C#{a[0]},#{a[1]}\
-              &markers=color:red%7Clabel:B%7C#{b[0]},#{b[1]}", class: "img-responsive center-block"
+              &markers=color:red%7Clabel:B%7C#{b[0]},#{b[1]}", class: "img-responsive #{position}-block"
   end
 end
